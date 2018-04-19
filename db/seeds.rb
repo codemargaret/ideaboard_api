fb1fa264b878f68adcc00a6792ba78e55e84a0bb
Margaret Berry <codeberry1@gmail.com>
2018-04-19 10:10:39 -0700

    add seeds and set up testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ideas = Idea.create(
+  [
+    {
+      title: "Band scheduling app",
+      body: "A Rails app to organize musicians"
+    },
+    {
+      title: "Show tracker app",
+      body: "A place to keep track of big name and local shows"
+    },
+    {
+      title: "Musical notation app",
+      body: "An app where you can write music and performance notes"
+    }
+  ])
